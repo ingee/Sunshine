9ef8b8bc4db5946667f0125e68fe63ad7eb74f4b
Kim, InGee <ingee@paran.com>
2016-06-20 00:01:12 +0900

    initial commit
    - make ListView layout for Sunshine-Weather-Forecast-Android-App

---
+++ app/src/main/java/com/android/ingee/sunshine/MainActivity.java
@@ -0,0 +1,40 @@
+package com.android.ingee.sunshine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayAdapter<String> mForecastAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] data = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 70/40",
+                "Weds - Cloudy - 72/63",
+                "Thurs - Asteroids - 75/65",
+                "Fri - Heavy Rain - 65/56",
+                "Sat - HELP TRAPPED IN WEATHERSTATION - 60/51",
+                "Sun - Sunny - 80/68"
+        };
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+        mForecastAdapter = new ArrayAdapter<String>(
+                this,
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast);
+        ListView vw = (ListView) findViewById(R.id.listview_forecast);
+        vw.setAdapter(mForecastAdapter);
+    }
+}
