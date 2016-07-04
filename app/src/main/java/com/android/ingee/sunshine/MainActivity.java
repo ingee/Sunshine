ac8116af68677923019764a721a0cdf08da05730
Kim, InGee <ingee@paran.com>
2016-07-05 06:16:30 +0900

    add 'Refresh' menu for Weather-REST-API call
    - but crashed due to permission problem

--- app/src/main/java/com/android/ingee/sunshine/MainActivity.java
+++ app/src/main/java/com/android/ingee/sunshine/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -45,6 +48,24 @@ public class MainActivity extends AppCompatActivity {
         ListView vw = (ListView) findViewById(R.id.listview_forecast);
         vw.setAdapter(mForecastAdapter);
         Log.e("MainActivity", "ingee,ingee,ingee~~~");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            weatherTask.execute();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
