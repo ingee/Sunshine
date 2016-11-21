eacda6642c5eb98a42f6283f951c37db5d9b64cd
Kim, InGee <ingee@paran.com>
2016-11-21 13:06:52 +0900

    query with column provisioning

--- app/src/main/java/com/android/ingee/sunshine/MainActivity.java
+++ app/src/main/java/com/android/ingee/sunshine/MainActivity.java
@@ -23,6 +23,27 @@ public class MainActivity
 
     private final String LOG_TAG = MainActivity.class.getSimpleName();
     private static final int FORECAST_LOADER = 0;
+    private static final String[] FORECAST_COLUMNS = {
+            WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+            WeatherContract.WeatherEntry.COLUMN_DATE,
+            WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+            WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING,
+            WeatherContract.WeatherEntry.COLUMN_WEATHER_ID,
+            WeatherContract.LocationEntry.COLUMN_COORD_LAT,
+            WeatherContract.LocationEntry.COLUMN_COORD_LONG
+    };
+    static final int COL_WEATHER_ID = 0;
+    static final int COL_WEATHER_DATE = 1;
+    static final int COL_WEATHER_DESC = 2;
+    static final int COL_WEATHER_MAX_TEMP = 3;
+    static final int COL_WEATHER_MIN_TEMP = 4;
+    static final int COL_LOCATION_SETTING = 5;
+    static final int COL_WEATHER_CONDITION_ID = 6;
+    static final int COL_COORD_LAT = 7;
+    static final int COL_COORD_LOG = 8;
+
     ForecastAdapter mForecastAdapter;
 
     private void updateWeather() {
@@ -112,7 +133,12 @@ public class MainActivity
         String sortOrder = WeatherContract.WeatherEntry.COLUMN_DATE + " ASC";
         Uri weatherForLocationUri = WeatherContract.WeatherEntry.buildWeatherLocationWithStartDate(
                 locationSetting, System.currentTimeMillis());
-        return new CursorLoader(this, weatherForLocationUri, null, null, null, sortOrder);
+        return new CursorLoader(this,
+                weatherForLocationUri,
+                FORECAST_COLUMNS,
+                null,
+                null,
+                sortOrder);
     }
 
     @Override
